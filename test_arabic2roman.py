48bd6e14b761ffca88e25d6c8da4cda22f214f45
Junwei LI <jlyc8@bu.edu>
2020-02-04 22:08:34 -0500

    final version

---
+++ test_arabic2roman.py
@@ -0,0 +1,19 @@
+from arabic2roman import int2roman
+
+import pytest
+
+def test_int2roman():
+
+  num=[1, 2, 3, 4, 10, 50, 90, 755]
+
+  roman= ["I", "II", "III", "IV", "X", "L", "XC", "DCCLV"]
+
+  for i in range(len(num)):
+
+    assert int2roman(num[i])== roman[i]
+
+# def main():
+#   test_int2roman()
+
+# if __name__ == '__main__':
+#   main()
